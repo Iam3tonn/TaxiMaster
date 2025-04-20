ccc56640f1f79b82d5da2d08b5adf2adbef2dd34
Dmitriy Krasnyh <dimathedevoloper@gmail.com>
2025-04-20 22:23:10 +0300

    Add files via upload

---
+++ ТаксиМастер/ТаксиМастер/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ТаксиМастер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnAddDriver = new System.Windows.Forms.Button();
+            this.btnEditDriver = new System.Windows.Forms.Button();
+            this.btnTrips = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lbSearch = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panelDrivers = new System.Windows.Forms.FlowLayoutPanel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.pbLogo);
+            this.pnlLogo.Location = new System.Drawing.Point(1, 2);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(856, 183);
+            this.pnlLogo.TabIndex = 1;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
+            this.pbLogo.Location = new System.Drawing.Point(110, 0);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(625, 180);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogo.TabIndex = 0;
+            this.pbLogo.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 946);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(861, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnAddDriver
+            // 
+            this.btnAddDriver.Location = new System.Drawing.Point(1, 870);
+            this.btnAddDriver.Name = "btnAddDriver";
+            this.btnAddDriver.Size = new System.Drawing.Size(211, 61);
+            this.btnAddDriver.TabIndex = 3;
+            this.btnAddDriver.Text = "Добавить водителя";
+            this.btnAddDriver.UseVisualStyleBackColor = true;
+            this.btnAddDriver.Click += new System.EventHandler(this.btnAddDriver_Click);
+            // 
+            // btnEditDriver
+            // 
+            this.btnEditDriver.Location = new System.Drawing.Point(218, 870);
+            this.btnEditDriver.Name = "btnEditDriver";
+            this.btnEditDriver.Size = new System.Drawing.Size(251, 61);
+            this.btnEditDriver.TabIndex = 4;
+            this.btnEditDriver.Text = "Редактировать водителя";
+            this.btnEditDriver.UseVisualStyleBackColor = true;
+            this.btnEditDriver.Click += new System.EventHandler(this.btnEditDriver_Click);
+            // 
+            // btnTrips
+            // 
+            this.btnTrips.Location = new System.Drawing.Point(475, 870);
+            this.btnTrips.Name = "btnTrips";
+            this.btnTrips.Size = new System.Drawing.Size(181, 61);
+            this.btnTrips.TabIndex = 5;
+            this.btnTrips.Text = "История поездок";
+            this.btnTrips.UseVisualStyleBackColor = true;
+            this.btnTrips.Click += new System.EventHandler(this.btnTrips_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(662, 870);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(195, 61);
+            this.btnRefresh.TabIndex = 6;
+            this.btnRefresh.Text = "Обновить список";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(153, 182);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(464, 26);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // lbSearch
+            // 
+            this.lbSearch.AutoSize = true;
+            this.lbSearch.Location = new System.Drawing.Point(12, 188);
+            this.lbSearch.Name = "lbSearch";
+            this.lbSearch.Size = new System.Drawing.Size(123, 20);
+            this.lbSearch.TabIndex = 8;
+            this.lbSearch.Text = "Поиск по ФИО:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(623, 180);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(234, 42);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panelDrivers
+            // 
+            this.panelDrivers.AutoScroll = true;
+            this.panelDrivers.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panelDrivers.Location = new System.Drawing.Point(1, 228);
+            this.panelDrivers.Name = "panelDrivers";
+            this.panelDrivers.Size = new System.Drawing.Size(888, 636);
+            this.panelDrivers.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 968);
+            this.Controls.Add(this.btnAddDriver);
+            this.Controls.Add(this.panelDrivers);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lbSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnTrips);
+            this.Controls.Add(this.btnEditDriver);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pnlLogo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Список водителей";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnAddDriver;
+        private System.Windows.Forms.Button btnEditDriver;
+        private System.Windows.Forms.Button btnTrips;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lbSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.FlowLayoutPanel panelDrivers;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ ТаксиМастер/ТаксиМастер/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ТаксиМастер
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrivers();
+        }
+        private void LoadDrivers(string search = "")
+        {
+            panelDrivers.Controls.Clear();
+
+            string query = @"SELECT DriverID, NameDriver, DateOfBirth, LicenseNumber, LicenseExpiryDate, 
+                            Address, PhoneNumber, Email, Rating, AvgRating, IsPremium
+                     FROM Drivers$
+                     WHERE NameDriver LIKE @search";
+
+            using (SqlConnection conn = new SqlConnection(Database.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@search", "%" + search + "%");
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int driverId = Convert.ToInt32(reader["DriverID"]);
+                    double avgRating = Convert.ToDouble(reader["AvgRating"]); // S
+                    int isPremium = Convert.ToBoolean(reader["IsPremium"]) ? 1 : 0; // P
+
+                    int totalTrips = GetCountFromQuery($"SELECT COUNT(*) FROM Trips$ WHERE DriverID = {driverId}");
+                    int completedTrips = GetCountFromQuery($"SELECT COUNT(*) FROM Trips$ WHERE DriverID = {driverId} AND Status = 'Завершена'");
+                    int totalViolations = GetCountFromQuery($"SELECT COUNT(*) FROM Violations$ WHERE DriverID = {driverId}");
+
+                    double violationCoef = DriverCalculator.CalculateViolationCoefficient(totalViolations, totalTrips);
+
+                    int C = totalTrips == 0 ? 0 : (int)Math.Round((double)completedTrips / totalTrips * 5); // К = от 0 до 5
+                    double A = 1.0 - violationCoef / 2.0; // Аккуратность
+                    int rating = DriverCalculator.CalculateRating(avgRating, C, A, isPremium);
+                    UpdateDriverRating(driverId, rating);
+
+
+                    // --- Создание карточки водителя ---
+                    var card = new Panel
+                    {
+                        Width = panelDrivers.Width - 30,
+                        Height = 160,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Margin = new Padding(5),
+                        BackColor = ColorTranslator.FromHtml("#CCFFFF"),
+                        Tag = driverId
+                    };
+
+                    var label = new Label
+                    {
+                        Text = $"ФИО: {reader["NameDriver"]}\n" +
+                               $"Дата рождения: {Convert.ToDateTime(reader["DateOfBirth"]).ToShortDateString()}\n" +
+                               $"ВУ: {reader["LicenseNumber"]} / {Convert.ToDateTime(reader["LicenseExpiryDate"]).ToShortDateString()}\n" +
+                               $"Телефон: {reader["PhoneNumber"]}\n" +
+                               $"Email: {reader["Email"]}\n" +
+                               $"Коэф. нарушений: {violationCoef}\n" +
+                               $"Рейтинг: {rating}",
+                        AutoSize = true,
+                        Location = new Point(10, 10),
+                        Font = new Font("Sitka Text", 9)
+                    };
+
+                    // --- Кнопка "Редактировать" ---
+                    var btnEdit = new Button
+                    {
+                        Text = "Редактировать",
+                        Size = new Size(110, 25),
+                        Location = new Point(card.Width - 250, 10),
+                        BackColor = Color.LightGray
+                    };
+                    btnEdit.Click += (s, e) =>
+                    {
+                        var form = new DriverForm(driverId);
+                        if (form.ShowDialog() == DialogResult.OK)
+                            LoadDrivers();
+                    };
+
+                    // --- Кнопка "История поездок" ---
+                    var btnTrips = new Button
+                    {
+                        Text = "История поездок",
+                        Size = new Size(120, 25),
+                        Location = new Point(card.Width - 120, 10),
+                        BackColor = Color.LightGray
+                    };
+                    btnTrips.Click += (s, e) =>
+                    {
+                        var form = new TripsForm(driverId);
+                        form.ShowDialog();
+                    };
+
+                    card.Controls.Add(label);
+                    card.Controls.Add(btnEdit);
+                    card.Controls.Add(btnTrips);
+                    panelDrivers.Controls.Add(card);
+                }
+            }
+        }
+
+        private void UpdateDriverRating(int driverId, int rating)
+        {
+            string query = "UPDATE Drivers$ SET Rating = @rating WHERE DriverID = @id";
+
+            using (SqlConnection conn = new SqlConnection(Database.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@rating", rating);
+                cmd.Parameters.AddWithValue("@id", driverId);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void btnAddDriver_Click(object sender, EventArgs e)
+        {
+            var form = new DriverForm();
+            if (form.ShowDialog() == DialogResult.OK)
+                LoadDrivers();
+        }
+
+        private void btnEditDriver_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Для редактирования выбери карточку водителя кликом по ней.", "Подсказка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void btnTrips_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (panelDrivers.Controls.Count == 0)
+                    return;
+
+                Panel selected = null;
+
+                // Пример: можно использовать текущую выделенную или кликать по одной
+                foreach (Control ctrl in panelDrivers.Controls)
+                {
+                    if (ctrl.Focused) selected = ctrl as Panel;
+                }
+
+                if (selected != null && selected.Tag != null)
+                {
+                    int driverId = Convert.ToInt32(selected.Tag);
+                    var form = new TripsForm(driverId);
+                    form.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Выбери водителя (клик по карточке), чтобы посмотреть его поездки.",
+                                    "Подсказка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при открытии истории поездок:\n" + ex.Message,
+                                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            LoadDrivers();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadDrivers(txtSearch.Text.Trim());
+        }
+        private int GetCountFromQuery(string query)
+        {
+            using (SqlConnection conn = new SqlConnection(Database.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Настройки формы
+            this.Text = "Список водителей";
+            this.BackColor = Color.White;
+
+            // Поле поиска (txtSearch)
+            txtSearch.BackColor = Color.White;
+            txtSearch.Font = new Font("Sitka Text", 9);
+
+            // Кнопка поиска (btnSearch)
+            btnSearch.Text = "Найти";
+            btnSearch.BackColor = ColorTranslator.FromHtml("#CCCCFF");
+            btnSearch.Font = new Font("Sitka Text", 9);
+            btnSearch.Click += (s, e2) => LoadDrivers(txtSearch.Text);
+
+            // FlowLayoutPanel (panelDrivers)
+            panelDrivers.BackColor = Color.WhiteSmoke;
+            panelDrivers.AutoScroll = true;
+            panelDrivers.WrapContents = true;
+
+            // Кнопки снизу
+            SetupButtonStyle(btnAddDriver, "Добавить водителя");
+            SetupButtonStyle(btnEditDriver, "Редактировать водителя");
+            SetupButtonStyle(btnTrips, "История поездок");
+            SetupButtonStyle(btnRefresh, "Обновить список");
+
+            btnAddDriver.Click += btnAddDriver_Click;
+            btnEditDriver.Click += btnEditDriver_Click;
+            btnTrips.Click += btnTrips_Click;
+            btnRefresh.Click += (s, e2) => LoadDrivers();
+
+            toolTip1.SetToolTip(txtSearch, "Введите ФИО для поиска водителя");
+            toolTip1.SetToolTip(btnSearch, "Найти водителя по ФИО");
+
+            toolTip1.SetToolTip(btnAddDriver, "Добавить нового водителя в систему");
+            toolTip1.SetToolTip(btnEditDriver, "Редактировать выбранного водителя");
+            toolTip1.SetToolTip(btnTrips, "Просмотреть историю поездок выбранного водителя");
+            toolTip1.SetToolTip(btnRefresh, "Обновить список водителей");
+            toolTip1.SetToolTip(panelDrivers, "Здесь отображаются все зарегистрированные водители");
+
+        }
+        private void SetupButtonStyle(Button btn, string text)
+        {
+            btn.Text = text;
+            btn.BackColor = ColorTranslator.FromHtml("#CCFFFF");
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.Font = new Font("Sitka Text", 9);
+        }
+
+    }
+}
